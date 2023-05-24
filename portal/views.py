31868a6564c626e3c518066bf1ac87a37285a468
Vinicius Moreira Alves <viniciusmoreiraalves@hotmail.com>
2023-05-23 23:17:04 -0300

    Colocando 'title' dinamico nas páginas

--- portal/views.py
+++ portal/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from portal.models import portal
 from utils.portal.factory import make_recipe
 
 
 def home(request):
-    posts = portal.objects.all().order_by('-id')
+    posts = portal.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'portal/pages/home.html', context={
         'posts': posts,
     })
 
 
-def contato(request, id):
-    return render(request, 'portal/pages/post-view.html', context={
-        'post': make_recipe(),
-        'is_detail_page': True,
+def category(request, category_id):
+    
+    posts = get_list_or_404(
+        portal.objects.filter(
+            Category__id=category_id,
+            is_published=True
+            ).order_by('-id')
+    )
+ 
+    return render(request, 'portal/pages/category.html', context={
+        'posts': posts,
+        'title': f'{posts.first().Category.name} -  Categoria',
     })
 
+
+def post_portal(request, id):
+
+    post = portal.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
+    post = get_object_or_404(portal, pk=id, is_published=True,)
+    
+    return render(request, 'portal/pages/post-view.html', context={
+    'post': post,
+    'is_detail_page': True,
+    })
