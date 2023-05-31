c3aa526e71860f97ff5f8d4d4dcd54a838b84bbb
Vinicius Moreira Alves <viniciusmoreiraalves@hotmail.com>
2023-05-31 19:59:11 -0300

    Criação de formulário para cadastro de usuario

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps criados
     'portal',
+    'authors',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portal.urls'))
+    path('', include('portal.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
